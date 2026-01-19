47e68ae55b631cf5d5e9682990b38d3764472759
Ashutosh! <ashutoshpatraybl@gmail.com>
2026-01-19 23:15:18 +0530

    Hosting Rituals

--- app_fixed.py
+++ app_fixed.py
@@ -276,5 +276,10 @@ def cleanup_session(session_id):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)      ## while testing in local machines
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
